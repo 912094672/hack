cb370d8e7bdc08ed599accb8b6b02d4a67571cc0
912094672 <912094672@qq.com>
2019-12-07 21:48:12 +0800

    identity page

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,12 +3,7 @@
 const app = getApp()
 
 Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
+  data: indexData,
   //事件处理函数
   bindViewTap: function() {
     wx.navigateTo({
@@ -16,32 +11,7 @@ Page({
     })
   },
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+    HandleLoad.bind(this)()
   },
   getUserInfo: function(e) {
     console.log(e)
@@ -52,3 +22,41 @@ Page({
     })
   }
 })
+
+function HandleLoad() {
+  if (app.globalData.userInfo) {
+    this.setData({
+      userInfo: app.globalData.userInfo,
+      hasUserInfo: true
+    })
+  } else if (this.data.canIUse) {
+    // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+    // 所以此处加入 callback 以防止这种情况
+    app.userInfoReadyCallback = res => {
+      this.setData({
+        userInfo: res.userInfo,
+        hasUserInfo: true
+      })
+    }
+  } else {
+    // 在没有 open-type=getUserInfo 版本的兼容处理
+    wx.getUserInfo({
+      success: res => {
+        app.globalData.userInfo = res.userInfo
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    })
+  }
+}
+exports.HandleLoad = HandleLoad
+
+const indexData = {
+  motto: 'Hello World',
+  userInfo: {},
+  hasUserInfo: false,
+  canIUse: wx.canIUse('button.open-type.getUserInfo')
+}
+exports.indexData = indexData
